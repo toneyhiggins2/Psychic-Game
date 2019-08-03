08ca16c0501d3bf5205a07f68e925d3aa5bb0a34
Toney Higgins <toneyhiggins2@gmail.com>
2019-08-03 16:48:27 -0400

    adding js code to complete calculations

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+
+    // Creates an var for the computer selected letter
+    var letterArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    // Creating variables to hold the number of wins, losses, and guesses left.
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 9;
+    var guessedSoFar = [];
+
+    // Create variables that hold references to the places in the HTML where we want to display things.
+    var winsText = document.getElementById("wins");
+    var lossesText = document.getElementById("losses");
+    var guessesLeftText = document.getElementById("guesses-left");
+    var guessedSoFarText = document.getElementById("guessed-sofar");
+
+    // This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {
+
+      // Determines which key was pressed.
+      var userGuess = event.key;
+      guessedSoFar.push(userGuess);
+      guessedSoFarText.textContent = guessedSoFar.join(" ");
+
+      // Randomly chooses a choice from the options array. This is the Computer's guess.
+      var computerGuess = letterArr[Math.floor(Math.random() * letterArr.length)];
+
+      winsText.textContent = wins;
+      lossesText.textContent = losses;
+      guessesLeftText.textContent = guessesLeft;
+
+ 
+      if (userGuess === computerGuess) {
+
+        wins++;
+        guessesLeft = 9;
+        guessedSoFar = [];
+
+      }
+      else {
+
+        guessesLeft--;
+      }
+
+      if (guessesLeft === 0) {
+          losses++;
+          guessesLeft = 9;
+          guessedSoFar = [];
+      }
+    };
+  
